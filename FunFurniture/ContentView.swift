a88d91e7ce39c17019ffad8eb969e0b830a50b5c
Chislaine Cruz <chislaine.cruz@gmail.com>
2020-06-26 15:06:11 -0400

    feat: added scrollbar with images to screen

--- FunFurniture/ContentView.swift
+++ FunFurniture/ContentView.swift
@@ -1,17 +1,21 @@
 //
 //  ContentView.swift
-//  FunFurniture
+
 //
 //  Created by Chislaine Cruz on 6/26/20.
 //  Copyright © 2020 Chislaine Cruz. All rights reserved.
-//FunFurniture
+//  FunFurniture
 
 import SwiftUI
 import RealityKit
 
 struct ContentView : View {
+    var models: [String] = ["chair_swan", "gramophone", "toy_biplane"]
     var body: some View {
-        Text("hello, hello, world. I am great!")
+        ZStack(alignment: .bottom) {
+            ARViewContainer()
+            ModelPickerView(models: self.models)
+        }
     }
 }
 
@@ -27,6 +31,32 @@ struct ARViewContainer: UIViewRepresentable {
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
+}
+
+struct ModelPickerView: View {
+    var models: [String]
+    
+    var body: some View {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 30){
+                    ForEach(0 ..< self.models.count){
+                        index in
+                        Button(action: {
+                            print("DEBUG: Selected model with name: \(self.models[index])")
+                        }) {
+                            Image(uiImage: UIImage(named: self.models[index])!)
+                            .resizable()
+                                .frame(height: 80)
+                                .aspectRatio(1/1, contentMode: .fit)
+                                .background(Color.white)
+                            .cornerRadius(12)
+                        }.buttonStyle(PlainButtonStyle())
+                    }
+                }
+            }
+        .padding(20)
+            .background(Color.black.opacity(0.4))
+    }
 }
 
 #if DEBUG
